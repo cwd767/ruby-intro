4fc3e7fe449c5dfaac70c6dcb04131feea28677f
Creighton <creighton.deyoung@kellogg.northwestern.edu>
2026-01-29 02:31:42 +0000

    section with more complex hashes has an error - check completed code

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,34 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile  = {
+  "name" => "Creighton", 
+  "location" => "KC", 
+  "status" => "student"
+}
+puts profile
 
 # Accessing data from the hash
+puts profile["name"]
+
+profile ["age"] = 44
+puts profile
+
+profile ["name"] = "Creighton Worthen DeYoung"
+puts profile
 
 # More Complex Hashes
+profile["location"] = { "city" => "Kansas City", "state" => "Kansas"} # hash within a hash
+puts profile
+
+puts profile["location"]["city"]
+
+timeline = [
+  { "status" => "in-class", "occured_at" => "8:30pm CT"},
+  { "status" => "gaming", "occured_at" => "10:30pm CT"},
+  { "status" => "sleeping", "occured_at" => "12:30am CT"},
+]
+
+profile["timeline"] = timeline
+puts profileputs "Right now!"
+puts profile["timeline"][0]["status"]
