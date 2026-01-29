75ea2e9ab736b5226560095cf0d1b1526a034b96
Creighton <creighton.deyoung@kellogg.northwestern.edu>
2026-01-29 01:30:19 +0000

    finished in-class intro to conditional logic

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,48 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+true
+false
 
 # Boolean Expressions
+puts 3 > 2
+puts 2 > 3
+puts 3 == 2
+puts 3 != 2
+puts 3 >= 2
 
 # If Conditional Logic
+if 3 > 2
+  puts "math checks out!"
+end
 
 # If/Else Conditional Logic
+if 3 <= 2
+  puts "math checks out!"
+else 
+  puts "yikes!"
+end
 
 # Elsif Conditional Logic
+home_team = 8
+away_team = 7
+
+if home_team > away_team
+  puts "We won!"
+elsif home_team == away_team
+  puts "It's a tie!"
+else 
+  puts "We lost :()"
+end
 
 # Combining Expressions
+temp = 8
+condition = "sunny"
+
+if temp > 29 && condition == "sunny"
+  puts "today is tolerable"
+end
+
+if temp > 29 || condition == "sunny"
+  puts "it's cold but at least the sun's out"
+end
