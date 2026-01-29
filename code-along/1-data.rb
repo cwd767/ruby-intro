b63424f71037a2d4572eab5092a772819ead1fb2
Creighton <creighton.deyoung@kellogg.northwestern.edu>
2026-01-29 01:10:07 +0000

    finished exercise #1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,45 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
-# ruby 1-data.rb
+# ruby code-along/1-data.rb
 
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3
+# "put out a string" - puts
+puts 2
+puts 2 + 3
+puts 2 - 3
+puts 5 * 3
+puts 5.0 / 2
+puts 5 % 3
 
 # Non-numbers
+puts "Hello World"
+puts 'Hello, ENTR-451'
 
 # True, False
+puts true 
+puts false
 
 # Nothing
 
-# Variables
+# Variables - should always be lowercase, use snake_case to differentiate between words
+x = 3
+y = 5
+puts x
+puts y 
+puts x * y
+
+food = "tacos"
 
 # Combine strings and variables
+puts food + " are delish!"
+puts "#{food} are super delicious"
+
+puts food * 3
+
 
 # String manipulation
+puts food.upcase ## this is an example of 'dot notation' which is a common way to interact with variables in programming languages
+
