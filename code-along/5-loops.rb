8b8b44556074200f417a7392964952f5ae0ff97b
Creighton <creighton.deyoung@kellogg.northwestern.edu>
2026-01-29 03:10:00 +0000

    completed in-class exercise

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -8,4 +8,32 @@
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+index = 0
+
+# puts "#{tacos[index]} tacos"
+# index = index + 1
+# puts "#{tacos[index]} tacos"
+
+loop do
+  if index == tacos.size
+    break
+  end
+
+  flavor = tacos[index]
+  puts "#{flavor} tacos"
+
+  index = index + 1
+end
+
+# the following is a short hand the above b/c it is so common; it will start at 0, determine the size, break when done, and run the loop
+# standard convention is to say for (singular) in (plural)
+
+# for _____ in dataset
+#   #....
+# end
+
+for zebra in tacos
+  puts "#{zebra} tacos"
+end
